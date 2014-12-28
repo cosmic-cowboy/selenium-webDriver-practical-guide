cb8553c598c3ab1dbf96d4f4b5bd1fad2e601482
hiroo takaku <slgerkamp@gmail.com>
2014-12-28 21:10:12 +0900

    chapter08 Selenium Grid：プラットフォームを指定

--- Learning-WebDriver/src/main/java/com/slgerkamp/selenium/chapter08/UsingSeleniumGrid.java
+++ Learning-WebDriver/src/main/java/com/slgerkamp/selenium/chapter08/UsingSeleniumGrid.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -13,8 +14,10 @@ import com.slgerkamp.selenium.lib.Utils;
 public class UsingSeleniumGrid {
 	public static void main(String[] args) throws MalformedURLException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
+		// ブラウザ指定
 		capabilities.setBrowserName("firefox");
-		
+		// プラットフォーム指定
+		capabilities.setPlatform(Platform.VISTA);
 		// Selenium Gridのハブにリクエストを発行
 		String url = "http://" + Utils.getProperty("hubRemoteWebDriverUrl") + "/wd/hub";
 		RemoteWebDriver remoteWebDriver = 
