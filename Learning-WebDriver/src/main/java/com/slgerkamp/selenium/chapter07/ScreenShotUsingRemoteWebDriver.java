398d27cb6895c7fd8e24df17a701d54876431448
hiroo takaku <slgerkamp@gmail.com>
2014-12-28 18:41:52 +0900

    chapter07 RemoteWebDriverとは：スクリーンショット

---
+++ Learning-WebDriver/src/main/java/com/slgerkamp/selenium/chapter07/ScreenShotUsingRemoteWebDriver.java
@@ -0,0 +1,33 @@
+package com.slgerkamp.selenium.chapter07;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.slgerkamp.selenium.lib.Utils;
+
+public class ScreenShotUsingRemoteWebDriver {
+	public static void main(String[] args) throws MalformedURLException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("firefox");
+		String url = "http://" + Utils.getProperty("remoteWebDriverUrl") + "/wd/hub";
+		RemoteWebDriver remoteWebDriver = 
+				new RemoteWebDriver(
+						new URL(url), capabilities);
+		
+		remoteWebDriver.get("http://google.com");
+		
+		WebElement searchBox = remoteWebDriver.findElement(By.name("q"));
+		searchBox.sendKeys("selenium");
+		searchBox.submit();
+		File srcFile  = ((TakesScreenshot)remoteWebDriver).getScreenshotAs(OutputType.FILE);
+		System.out.println(srcFile.getAbsolutePath());
+	}
+}
